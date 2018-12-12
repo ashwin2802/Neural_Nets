690cdc40ea1806ec1468d42509ca451ccc721c0d
Ashwin Shenai <ashwins@iitk.ac.in>
2018-12-12 20:19:14 +0530

    completed prediction script

--- nn_mnist/scripts/predict.py
+++ nn_mnist/scripts/predict.py
@@ -5,10 +5,6 @@ from keras.utils import to_categorical
 #import MNIST database
 from keras.datasets import mnist
 
-#function to allow single image input in the predict function
-def prep(x):
-    x = np.expand_dims(x,axis=0)
-
 #download the MNIST data
 (X,Y),(x_t,y_t) = mnist.load_data()
 print("Loaded database.")
@@ -30,12 +26,28 @@ x_t = x_t.reshape(10000,28,28,1)
 y = to_categorical(Y)
 Y_t = to_categorical(y_t)
 
+#extract predicted label from model.predict
+def extract(arp):
+    arr = list(np.array(list(arp[0].flat)))
+    for i in range(len(arr)):
+        if(arr[i]==max(arr)):
+            return i
+
+#output predicted label and compare with database label
+def output(a, b):
+    print("The digit in the image is " + str(a))
+    print("The label of the image is " + str(b))
+    if(a==b):
+        print("Prediction successful.")
+    else:
+        print("Prediction failed.")
+
 #prediction function with error handling
-## incomplete!
 def predict():
     num = int(input("Enter the ID of image to test: "))
     if(num<10001 and num>0):
-        model.predict(prep(x_t[num]))
+        out = model.predict(x_t[num-1][None,:,:,:])
+        output(extract(out), y_t[num-1])
     else:
         print("Out of range. Please enter a number between 1 and 10000.")
         predict()
@@ -51,4 +63,5 @@ def run():
         run()
 
 run()
+
 
